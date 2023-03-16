c0b2eb2a0c5097e0c9d25f569d57966f6619ec4b
Dumbo-Lincoln <dumbiIincoIn@gmail.com>
2023-03-15 18:04:39 -0700

    coding v3

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.lang.foreign.Addressable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,10 +20,8 @@ class Scanner {
     Scanner(String source) {
         this.source = source;
     }
-
-
     List<Token> scanTokens() {
-        while (!isAtEnd)) {
+        while (!isAtEnd()) {
             start = current;
             scanToken();
         }
@@ -32,5 +31,38 @@ class Scanner {
     }
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN) ; break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT) ; break;
+            case '-': addToken(MINUS) ; break ;
+            case '+': addToken(PLUS) ; break ;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
     }
 }
